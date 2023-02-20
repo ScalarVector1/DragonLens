a6db390e1f33ff53573c9e274c37391b83e48425
bobfishkins23 <bobfishkins23@yahoo.com>
2023-02-20 18:23:09 -0500

    MOTD

---
+++ Core/Systems/MOTDSystem.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace DragonLens.Core.Systems
+{
+	/// <summary>
+	/// Displays a welcome message to the user
+	/// </summary>
+	internal class MOTDPlayer : ModPlayer
+	{
+		public override void OnEnterWorld(Player player)
+		{
+			string MOTD = $"Thank you for using DragonLens V.{Mod.Version}!\n\n" +
+				$"Be sure to check the customize tool (wrench icon) to set up the mod with the tools you need.\n\n" +
+				$"Additional documentation can be found at https://github.com/ScalarVector1/DragonLens/wiki";
+
+			Main.NewText(MOTD, new Color(200, 235, 255));
+		}
+	}
+}
