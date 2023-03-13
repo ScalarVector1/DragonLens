454b8fc42979661a51380261f36058d7207c78f5
bobfishkins23 <bobfishkins23@yahoo.com>
2023-03-13 13:25:50 -0400

    Journey mode support

--- Content/Tools/Gameplay/Difficulty.cs
+++ Content/Tools/Gameplay/Difficulty.cs
@@ -2,16 +2,25 @@
 using Microsoft.Xna.Framework;
 using Terraria;
 using Terraria.ID;
+using Terraria.ModLoader;
 
 namespace DragonLens.Content.Tools.Gameplay
 {
 	internal class Difficulty : Tool
 	{
+		public static bool originallyJourney = false;
+
+		public static int oldPlayerDifficulty = -1;
+
 		public override string IconKey => "Difficulty";
 
 		public override string DisplayName => "Difficulty switcher";
 
-		public override string Description => "Cycle through the 3 main game difficulties";
+		public override string Description => "Cycle through the 3 main game difficulties NEWBLOCK Right click to toggle journey mode";
+
+		public override bool HasRightClick => true;
+
+		public override string RightClickName => "Toggle journey mode";
 
 		public override void OnActivate()
 		{
@@ -30,6 +39,45 @@ namespace DragonLens.Content.Tools.Gameplay
 				Main.GameMode = GameModeID.Normal;
 				Main.NewText("The game is now in normal mode.", new Color(180, 180, 255));
 			}
+		}
+
+		public override void OnRightClick()
+		{
+			if (Main.LocalPlayer.difficulty != 3)
+			{
+				oldPlayerDifficulty = Main.LocalPlayer.difficulty;
+				Main.LocalPlayer.difficulty = 3;
+				Main.NewText("Journey mode enabled.", Main.creativeModeColor);
+			}
+			else
+			{
+				if (oldPlayerDifficulty == -1 || oldPlayerDifficulty == 3)
+					oldPlayerDifficulty = 0;
+
+				Main.LocalPlayer.difficulty = (byte)oldPlayerDifficulty;
+				Main.NewText("Journey mode disabled.", Color.LightGray);
+			}
+		}
+	}
+
+	/// <summary>
+	/// This prevents the player from mutating the journey state of their save
+	/// </summary>
+	internal class JourneySafetyPlayer : ModPlayer
+	{
+		public override void OnEnterWorld(Player player)
+		{
+			if (player.difficulty == 3)
+				Difficulty.originallyJourney = true;
+		}
+
+		public override void PreSavePlayer()
+		{
+			if (Difficulty.originallyJourney && Main.LocalPlayer.difficulty != 3)
+				Main.LocalPlayer.difficulty = 3;
+
+			if (!Difficulty.originallyJourney && Main.LocalPlayer.difficulty == 3)
+				Main.LocalPlayer.difficulty = (byte)Difficulty.oldPlayerDifficulty;
 		}
 	}
 }
