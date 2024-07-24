396f9c2186f1c2cf2407ccf38e0953258346c44c
bobfishkins23 <bobfishkins23@yahoo.com>
2024-07-24 00:04:25 -0400

    Inventory management prototype

---
+++ Content/Tools/Multiplayer/InventoryManager.cs
@@ -0,0 +1,247 @@
+﻿using DragonLens.Content.GUI;
+using DragonLens.Content.GUI.FieldEditors;
+using DragonLens.Core.Loaders.UILoading;
+using DragonLens.Core.Systems.ThemeSystem;
+using DragonLens.Core.Systems.ToolSystem;
+using DragonLens.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.GameContent.UI.Elements;
+using Terraria.ModLoader.UI.Elements;
+using Terraria.UI;
+
+namespace DragonLens.Content.Tools.Multiplayer
+{
+	internal class InventoryManagerWindow : DraggableUIState
+	{
+		public Player player;
+
+		public UIGrid slots = new();
+		public StyledScrollbar slotScroll;
+
+		public override Rectangle DragBox => new((int)basePos.X, (int)basePos.Y, 580, 32);
+
+		public override Vector2 DefaultPosition => new(0.5f, 0.5f);
+
+		public override int InsertionIndex(List<GameInterfaceLayer> layers)
+		{
+			return layers.FindIndex(layer => layer.Name.Equals("Vanilla: Mouse Text"));
+		}
+
+		public override void SafeOnInitialize()
+		{
+			width = 580;
+			height = 456;
+
+			slotScroll = new(UserInterface);
+			slotScroll.Width.Set(32, 0);
+			slotScroll.Height.Set(380, 0);
+			Append(slotScroll);
+
+			slots = new();
+			slots.Width.Set(530, 0);
+			slots.Height.Set(380, 0);
+			slots.SetScrollbar(slotScroll);
+			Append(slots);
+		}
+
+		public override void AdjustPositions(Vector2 newPos)
+		{
+			slots.Left.Set(newPos.X + 10, 0);
+			slots.Top.Set(newPos.Y + 64, 0);
+
+			slotScroll.Left.Set(newPos.X + 542, 0);
+			slotScroll.Top.Set(newPos.Y + 64, 0);
+		}
+
+		public override void Draw(SpriteBatch spriteBatch)
+		{
+			var target = new Rectangle((int)basePos.X, (int)basePos.Y, 580, 456);
+
+			GUIHelper.DrawBox(spriteBatch, target, ThemeHandler.BackgroundColor);
+
+			Texture2D back = Assets.GUI.Gradient.Value;
+			var backTarget = new Rectangle((int)basePos.X + 8, (int)basePos.Y + 8, 450, 48);
+			spriteBatch.Draw(back, backTarget, Color.Black * 0.5f);
+
+			Texture2D gridBack = Terraria.GameContent.TextureAssets.MagicPixel.Value;
+			var gridBackTarget = slots.GetDimensions().ToRectangle();
+			gridBackTarget.Inflate(4, 4);
+			spriteBatch.Draw(gridBack, gridBackTarget, Color.Black * 0.25f);
+
+			Texture2D icon = ThemeHandler.GetIcon("PlayerManager");
+			spriteBatch.Draw(icon, basePos + Vector2.One * 16, Color.White);
+
+			Utils.DrawBorderStringBig(spriteBatch, $"{player.name}'s Inventory", basePos + new Vector2(icon.Width + 24, 16), Color.White, 0.6f);
+
+			base.Draw(spriteBatch);
+		}
+
+		public void SetInventory()
+		{
+			slots.Clear();
+
+			for (int k = 0; k < player.inventory.Length; k++)
+			{
+				slots.Add(new ItemSlot(player.inventory, k, 0));
+			}
+
+			slots.Add(new Seperator("Piggy bank", 1));
+
+			for (int k = 0; k < player.bank.item.Length; k++)
+			{
+				slots.Add(new ItemSlot(player.bank.item, k, 2));
+			}
+
+			slots.Add(new Seperator("Safe", 3));
+
+			for (int k = 0; k < player.bank2.item.Length; k++)
+			{
+				slots.Add(new ItemSlot(player.bank2.item, k, 4));
+			}
+
+			slots.Add(new Seperator("Defenders forge", 5));
+
+			for (int k = 0; k < player.bank3.item.Length; k++)
+			{
+				slots.Add(new ItemSlot(player.bank3.item, k, 6));
+			}
+
+			slots.Add(new Seperator("Void vault", 7));
+
+			for (int k = 0; k < player.bank4.item.Length; k++)
+			{
+				slots.Add(new ItemSlot(player.bank4.item, k, 8));
+			}
+		}
+	}
+
+	internal class Seperator : SmartUIElement
+	{
+		string text;
+		public int invIndex;
+
+		public Seperator(string text, int invIndex)
+		{
+			this.text = text;
+			this.invIndex = invIndex;
+
+			Width.Set(520, 0);
+			Height.Set(24, 0);
+		}
+
+		public override void Draw(SpriteBatch spriteBatch)
+		{
+			Texture2D back = Assets.GUI.Gradient.Value;
+			var backTarget = GetDimensions().ToRectangle();
+			spriteBatch.Draw(back, backTarget, Color.Black * 0.5f);
+
+			Utils.DrawBorderString(spriteBatch, text, GetDimensions().Position() + new Vector2(12, 4), Color.White, 0.8f);
+		}
+
+		public override int CompareTo(object obj)
+		{
+			if (obj is ItemSlot slot)
+			{
+				return invIndex.CompareTo(slot.invIndex);
+			}
+
+			if (obj is Seperator sep)
+			{
+				return invIndex.CompareTo(sep.invIndex);
+			}
+
+			return base.CompareTo(obj);
+		}
+	}
+
+	internal class ItemSlot : SmartUIElement
+	{
+		Item[] inventory;
+		int index;
+		public int invIndex;
+
+		Item item => inventory[index];
+
+		public bool noItem => item is null || item.IsAir;
+
+		public ItemSlot(Item[] inventory, int index, int invIndex)
+		{
+			this.inventory = inventory;
+			this.index = index;
+			this.invIndex = invIndex;
+
+			Width.Set(48, 0);
+			Height.Set(48, 0);
+		}
+
+		public override void SafeClick(UIMouseEvent evt)
+		{
+			if (!Main.mouseItem.IsAir && noItem)
+			{
+				var newItem = Main.mouseItem.Clone();
+				inventory[index] = newItem;
+
+				Main.LocalPlayer.HeldItem.TurnToAir();
+				Main.mouseItem.TurnToAir();
+			}
+			else if (Main.mouseItem.IsAir && !noItem)
+			{
+				Main.mouseItem = item.Clone();
+				item.TurnToAir();
+			}
+			else if (!Main.mouseItem.IsAir && !noItem)
+			{
+				var temp = item.Clone();
+
+				var newItem = Main.mouseItem.Clone();
+				inventory[index] = newItem;
+
+				Main.mouseItem = temp;
+			}
+
+			ModContent.GetInstance<PlayerManager>().SendItem(UILoader.GetUIState<InventoryManagerWindow>().player.whoAmI, index, invIndex, item);
+		}
+
+		public override void Draw(SpriteBatch spriteBatch)
+		{
+			GUIHelper.DrawBox(spriteBatch, GetDimensions().ToRectangle(), ThemeHandler.ButtonColor);
+
+			if (!noItem)
+			{
+				Main.inventoryScale = 36 / 52f * 48 / 36f;
+				var clone = item.Clone();
+				Terraria.UI.ItemSlot.Draw(spriteBatch, ref clone, 21, GetDimensions().Position());
+
+				if (IsMouseHovering)
+				{
+					Main.LocalPlayer.mouseInterface = true;
+					Main.HoverItem = item.Clone(); // Fix knockback issue. Issue #73
+					Main.hoverItemName = "a";
+				}
+			}
+		}
+
+		public override int CompareTo(object obj)
+		{
+			if (obj is ItemSlot slot)
+			{
+				if (invIndex != slot.invIndex)
+					return invIndex.CompareTo(slot.invIndex);
+
+				return index.CompareTo(slot.index);
+			}
+
+			if (obj is Seperator sep)
+			{
+				return invIndex.CompareTo(sep.invIndex);
+			}
+
+			return base.CompareTo(obj);
+		}
+	}
+}
--- Content/Tools/Multiplayer/PlayerManager.cs
+++ Content/Tools/Multiplayer/PlayerManager.cs
@@ -1,6 +1,4 @@
 ﻿using DragonLens.Content.GUI;
-using DragonLens.Content.Tools.Gameplay;
-using DragonLens.Content.Tools.Visualization;
 using DragonLens.Core.Loaders.UILoading;
 using DragonLens.Core.Systems;
 using DragonLens.Core.Systems.ThemeSystem;
@@ -9,9 +7,6 @@ using DragonLens.Helpers;
 using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Terraria.GameContent.UI.Elements;
 using Terraria.ID;
 using Terraria.ModLoader.IO;
@@ -21,6 +16,11 @@ namespace DragonLens.Content.Tools.Multiplayer
 {
 	internal class PlayerManager : Tool
 	{
+		public int player;
+		public int index;
+		public int inventory;
+		public Item item;
+
 		public override string IconKey => "PlayerManager";
 
 		public override void OnActivate()
@@ -30,6 +30,55 @@ namespace DragonLens.Content.Tools.Multiplayer
 
 			if (state.visible)
 				state.SetPlayers();
+		}
+
+		public override void SendPacket(BinaryWriter writer)
+		{
+			if (item is null)
+				return;
+
+			writer.Write(player);
+			writer.Write(index);
+			writer.Write(inventory);
+			ItemIO.Send(item, writer, true);
+		}
+
+		public override void RecievePacket(BinaryReader reader, int sender)
+		{
+			int pIndex = reader.ReadInt32();
+			Player player = Main.player[pIndex];
+
+			int index = reader.ReadInt32();
+			int invIndex = reader.ReadInt32();
+
+			Item[] inventory = invIndex switch
+			{
+				0 => player.inventory,
+				2 => player.bank.item,
+				4 => player.bank2.item,
+				6 => player.bank3.item,
+				8 => player.bank4.item,
+				_ => player.inventory
+			};
+
+			Item item = ItemIO.Receive(reader, true);
+			inventory[index] = item.Clone();
+
+			if (Main.netMode == NetmodeID.Server)
+				SendItem(pIndex, index, invIndex, item, sender);
+		}
+
+		public void SendItem(int player, int index, int inventory, Item item, int ignore = -1)
+		{
+			if (item is null)
+				return;
+
+			this.player = player;
+			this.index = index;
+			this.inventory = inventory;
+			this.item = item.Clone();
+
+			NetSend(-1, ignore);
 		}
 	}
 
@@ -48,7 +97,7 @@ namespace DragonLens.Content.Tools.Multiplayer
 
 	internal class PlayerManagerWindow : DraggableUIState
 	{
-		public UIList playerList = new();
+		public UIList playerList = [];
 		public StyledScrollbar playerScroll;
 
 		public override Rectangle DragBox => new((int)basePos.X, (int)basePos.Y, 400, 32);
@@ -70,7 +119,7 @@ namespace DragonLens.Content.Tools.Multiplayer
 			playerScroll.Height.Set(320, 0);
 			Append(playerScroll);
 
-			playerList = new();
+			playerList = [];
 			playerList.Width.Set(320, 0);
 			playerList.Height.Set(500, 0);
 			playerList.SetScrollbar(playerScroll);
@@ -113,7 +162,7 @@ namespace DragonLens.Content.Tools.Multiplayer
 		{
 			playerList.Clear();
 
-			foreach(Player player in Main.ActivePlayers)
+			foreach (Player player in Main.ActivePlayers)
 			{
 				playerList.Add(new PlayerManagerEntry(player));
 			}
@@ -129,34 +178,50 @@ namespace DragonLens.Content.Tools.Multiplayer
 			this.player = player;
 
 			Width.Set(300, 0);
-			Height.Set(80, 0);
+			Height.Set(90, 0);
 
-			var admin = new PlayerManagerButton("Give Admin", 100);
+			var admin = new PlayerManagerButton("Toggle Admin", "Gives or revokes admin permissions from this player. Make sure this is someone you can trust! Admins can use all features of DragonLens, including this one!", Assets.GUI.AdminIcon, () => PermissionHandler.CanUseTools(player));
 			admin.Left.Set(10, 0);
-			admin.Top.Set(40, 0);
+			admin.Top.Set(42, 0);
 			admin.OnLeftClick += (a, b) => ToggleAdmin();
 			Append(admin);
 
-			var kick = new PlayerManagerButton("Kick", 80);
-			kick.Left.Set(120, 0);
-			kick.Top.Set(40, 0);
+			var kick = new PlayerManagerButton("Kick", "Forcibly disconnect this player from the game", Assets.GUI.KickIcon, () => false);
+			kick.Left.Set(58, 0);
+			kick.Top.Set(42, 0);
 			kick.OnLeftClick += (a, b) => Kick();
 			Append(kick);
 
-			var stalk = new PlayerManagerButton("View", 80);
-			stalk.Left.Set(210, 0);
-			stalk.Top.Set(40, 0);
+			var stalk = new PlayerManagerButton("View", "Move the camera to this players position", Assets.GUI.StalkIcon, () => ModContent.GetInstance<PlayerManagerSystem>().stalkedPlayer == player);
+			stalk.Left.Set(106, 0);
+			stalk.Top.Set(42, 0);
 			stalk.OnLeftClick += (a, b) => Stalk();
 			Append(stalk);
+
+			var inv = new PlayerManagerButton("Inventory", "View and edit this player's inventories", Assets.GUI.StalkIcon, () => false);
+			inv.Left.Set(154, 0);
+			inv.Top.Set(42, 0);
+			inv.OnLeftClick += (a, b) => OpenInventory();
+			Append(inv);
 		}
 
 		public override void Draw(SpriteBatch spriteBatch)
 		{
 			var dims = GetDimensions().ToRectangle();
-			GUIHelper.DrawBox(spriteBatch, dims, ThemeHandler.ButtonColor);
+			GUIHelper.DrawBox(spriteBatch, dims, ThemeHandler.BackgroundColor);
+
+			Texture2D back = Assets.GUI.Gradient.Value;
+			var backTarget = new Rectangle(dims.Left + 4, dims.Top + 4, 260, 34);
+			spriteBatch.Draw(back, backTarget, Color.Black * 0.5f);
 
 			Main.PlayerRenderer.DrawPlayerHead(Main.Camera, player, dims.TopLeft() + Vector2.One * 20);
-			Utils.DrawBorderString(spriteBatch, player.name, dims.TopLeft() + new Vector2(46, 12), PermissionHandler.CanUseTools(player) ? Color.Cyan : Color.White);
+			Utils.DrawBorderString(spriteBatch, player.name, dims.TopLeft() + new Vector2(46, 12), PermissionHandler.CanUseTools(player) ? new Color(100, 235, 235) : Color.White);
+
+			Texture2D teamIcon = Terraria.GameContent.TextureAssets.Pvp[1].Value;
+			var source = new Rectangle(player.team * 18, 0, 18, 18);
+			spriteBatch.Draw(teamIcon, dims.TopLeft() + new Vector2(272, 12), source, Color.White);
+
+			Utils.DrawBorderString(spriteBatch, $"{Math.Round(player.Center.X / 16)}, {Math.Round(player.Center.Y / 16)}", dims.TopLeft() + new Vector2(200, 52), Color.Gray);
 
 			base.Draw(spriteBatch);
 		}
@@ -164,12 +229,24 @@ namespace DragonLens.Content.Tools.Multiplayer
 		public void ToggleAdmin()
 		{
 			// Theoretically redundant but this is probably the most important button to protect
-			if (Core.Systems.PermissionHandler.CanUseTools(Main.LocalPlayer))
+			if (PermissionHandler.CanUseTools(Main.LocalPlayer))
 			{
-				if (Core.Systems.PermissionHandler.CanUseTools(player))
-					Core.Systems.PermissionHandler.RemoveAdmin(player);
+				if (player == Main.LocalPlayer)
+				{
+					Main.NewText("You cannot remove your own permissions!", Color.Red);
+					return;
+				}
+
+				if (PermissionHandler.CanUseTools(player))
+				{
+					PermissionHandler.RemoveAdmin(player);
+					Main.NewText($"{player.name} is no longer an admin.", Color.Yellow);
+				}
 				else
-					Core.Systems.PermissionHandler.AddAdmin(player);
+				{
+					PermissionHandler.AddAdmin(player);
+					Main.NewText($"{player.name} is now an admin.", Color.Yellow);
+				}
 			}
 		}
 
@@ -185,25 +262,48 @@ namespace DragonLens.Content.Tools.Multiplayer
 			else
 				ModContent.GetInstance<PlayerManagerSystem>().stalkedPlayer = null;
 		}
+
+		public void OpenInventory()
+		{
+			UILoader.GetUIState<InventoryManagerWindow>().player = player;
+			UILoader.GetUIState<InventoryManagerWindow>().SetInventory();
+			UILoader.GetUIState<InventoryManagerWindow>().visible = true;
+		}
 	}
 
 	internal class PlayerManagerButton : SmartUIElement
 	{
-		public string text;
+		public string name;
+		public string tooltip;
+		public Asset<Texture2D> icon;
 
-		public PlayerManagerButton(string text, int width)
+		public Func<bool> active;
+
+		public PlayerManagerButton(string name, string tooltip, Asset<Texture2D> icon, Func<bool> active)
 		{
-			this.text = text;
-			Width.Set(width, 0);
-			Height.Set(30, 0);
+			this.name = name;
+			this.tooltip = tooltip;
+			this.icon = icon;
+			this.active = active;
+
+			Width.Set(40, 0);
+			Height.Set(40, 0);
 		}
 
 		public override void Draw(SpriteBatch spriteBatch)
 		{
 			var dims = GetDimensions().ToRectangle();
 			GUIHelper.DrawBox(spriteBatch, dims, ThemeHandler.ButtonColor);
+			spriteBatch.Draw(icon.Value, dims.TopLeft() + Vector2.One * 4, Color.White);
 
-			Utils.DrawBorderString(spriteBatch, text, dims.Center() + Vector2.UnitY * 4, Color.White, 0.8f, 0.5f, 0.5f);
+			if (active())
+				GUIHelper.DrawOutline(spriteBatch, dims, ThemeHandler.ButtonColor.InvertColor());
+
+			if (IsMouseHovering)
+			{
+				Tooltip.SetName(name);
+				Tooltip.SetTooltip(tooltip);
+			}
 		}
 	}
 }
