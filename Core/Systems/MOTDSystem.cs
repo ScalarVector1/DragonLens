4178acbd176c394fb82b807f62d8d5cb86a2a0d3
bobfishkins23 <bobfishkins23@yahoo.com>
2025-03-02 11:54:38 -0500

    MotD only on update and smaller

--- Core/Systems/MOTDSystem.cs
+++ Core/Systems/MOTDSystem.cs
@@ -1,4 +1,6 @@
 using DragonLens.Helpers;
+using System;
+using Terraria.ModLoader.IO;
 
 namespace DragonLens.Core.Systems
 {
@@ -7,11 +9,25 @@ namespace DragonLens.Core.Systems
 	/// </summary>
 	internal class MOTDPlayer : ModPlayer
 	{
+		public Version seenMotd;
+
 		public override void OnEnterWorld()
 		{
-			string MOTD = LocalizationHelper.GetText("MOTD", Mod.Version);
+			if (Mod.Version > seenMotd)
+			{
+				string MOTD = LocalizationHelper.GetText("MOTD", Mod.Version);
+				Main.NewText(MOTD, new Color(200, 235, 255));
+			}
+		}
 
-			Main.NewText(MOTD, new Color(200, 235, 255));
+		public override void SaveData(TagCompound tag)
+		{
+			tag["seenMotd"] = seenMotd.ToString();
+		}
+
+		public override void LoadData(TagCompound tag)
+		{
+			seenMotd = Version.Parse(tag.GetString("seenMotd"));
 		}
 	}
 }
