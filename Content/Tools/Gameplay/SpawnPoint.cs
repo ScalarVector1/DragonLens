2c0501cecb7e5665fddb5d153b947db44cb3d29b
shnakamura <joaovtnakamura@gmail.com>
2026-08-19 01:50:17 -0300

    feat: spawn point tool

---
+++ Content/Tools/Gameplay/SpawnPoint.cs
@@ -0,0 +1,20 @@
+﻿using DragonLens.Core.Systems.ToolSystem;
+using DragonLens.Helpers;
+
+namespace DragonLens.Content.Tools.Gameplay
+{
+	public sealed class SpawnPoint : Tool
+	{
+		private static Player Player => Main.LocalPlayer;
+	
+		public override string IconKey => "SpawnPoint";
+
+		public override void OnActivate()
+		{
+			Main.spawnTileX = (int)((Player.position.X - 8f + Player.width / 2f) / 16f);
+			Main.spawnTileY = (int)((Player.position.Y + Player.height) / 16f);
+		
+			Main.NewText(LocalizationHelper.GetToolText("SpawnPoint.Message", Main.spawnTileX, Main.spawnTileY));
+		}
+	}
+}
